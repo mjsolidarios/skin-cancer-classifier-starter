ebafb80d8ec5ee8d3a3b8bcd681bae28c586907a
GemWinnnn <126636338+GemWinnnn@users.noreply.github.com>
2025-03-14 23:50:48 +0800

    redo

--- CANETE/app.py
+++ CANETE/app.py
@@ -117,4 +117,4 @@ def predict():
         return jsonify({'error': 'An error occurred during prediction.'}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
